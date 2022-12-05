c9f5724fb883db9e5c804345e95dd8617fae4b3c
N1colas7 <nicolasnd9897@gmail.com>
2022-12-05 15:34:04 +0100

    fix and finish exercise

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
 const list = [
-    'nutella',
-    'pane',
+    'Nutella',
+    'Guanciale',
     'acqua',
     'zucchero',
     'sale',
     'pomodoro',
     'pasta'
 ]
+
+const listContainer = document.querySelector('.lista-spesa');
+ 
+let element =  '';
+let i = 0;
+while (i < list.length) {   
+    let item = list[i];
+
+    let element = `<li class="list-item">${item}</li>` 
+    listContainer.innerHTML += element ;
+
+
+
+    i++;
+    
+
+}
